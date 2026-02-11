cda2f9245d1fedcd71efc6f71eaf29d9c2a62bb4
cherligan <259553831+cherligan@users.noreply.github.com>
2026-02-11 23:51:20 +0300

    edit note

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react"
 import Header from "./components/Header"
 import Notes from "./components/Note/Notes"
+import "./index.css"
+import "./components/Button/Button.css"
 export default function App() {
-  const [notes, setNotes] = useState([])
+  const [notes, setNotes] = useState([
+    {
+      id: Date.now(),
+      title: "простая заметка"
+    }
+  ])
+  const [overlayOpen, setOverlayOpen] = useState(false)
+  const [activeNoteId, setActiveNoteId] = useState(null)
   const addNote = () => {
     const newNote = {
       id: Date.now(),
@@ -10,11 +19,36 @@ export default function App() {
     }
     setNotes(prev => [...prev, newNote])
   }
+  const openOverlay = (id) => 
+  {
+    setActiveNoteId(id)
+    setOverlayOpen(true)
+  }
+  const closeOverlay = () => 
+  {
+    setActiveNoteId(null)
+    setOverlayOpen(false)
+  }
   return (
     <div>
       <Header />
       <main>
-          <Notes notes={notes} addNote={addNote}/>
+          <Notes notes={notes} addNote={addNote} onCardClick={openOverlay}/>
+          {overlayOpen && 
+          (
+            <div className="overlay" onClick={closeOverlay}>
+              <div className="modal" onClick={e => e.stopPropagation()}>
+                <h1>Редактирование {activeNoteId} </h1>
+                <input type="text" placeholder="Название" />
+                <textarea placeholder="Текст" />
+                <div className="row">
+                  <button className="button" onClick={closeOverlay}>Сохранить</button>
+                  <button className="button" onClick={closeOverlay}>Закрыть</button>
+                </div>
+                
+              </div>
+            </div>
+          )}
       </main>
     </div>
   )
--- src/components/Note/Notes.jsx
+++ src/components/Note/Notes.jsx
@@ -1,10 +1,10 @@
 import "./Note.css"
-export default function Notes({ notes, addNote})
+export default function Notes({ notes, addNote, onCardClick })
 {
   return(
     <div className="notes-grid"> 
         {notes.map(note => (
-            <div key={note.id} className="card-note">{note.title}</div>
+            <div key={note.id} className="card-note" onClick={() => onCardClick(note.id)}>{note.title}</div>
         ))}
         <div className="empty-note" onClick={addNote}> + </div>
     </div>
